f0704ef7071061036befd7722422c3792cf71e89
Chiara Paci <chiara.paci@gmail.com>
2014-12-02 11:43:58 +0100

    Add sc_editbox 12.1

--- santaclara_base/static/santaclara_base/jquery-ui-santaclara-widgets.js
+++ santaclara_base/static/santaclara_base/jquery-ui-santaclara-widgets.js
@@ -259,6 +259,7 @@
 	    edit_class: "santa_clara_edit",
 	    delete_class: "santa_clara_delete",
 	    save_class: "santa_clara_save",
+	    delete_show_always: false,
 	    delete_dom_function: function(elem,delete_target_dom_id){
 		$("#"+delete_target_dom_id).remove();
 	    },
@@ -290,7 +291,10 @@
 	    target_view.show();
 	    edit_buttons.show();
 	    save_buttons.hide();
-	    delete_buttons.hide();
+	    if (opts.delete_show_always && opts.deletable)
+		delete_buttons.show();
+	    else
+		delete_buttons.hide();
 
 	    /** edit **/
 	    edit_buttons.click(function(event){
